b943f4f92ea5eedf7108ad44e8c40cfd6ab09199
Nemanja Malinović <97406287+nemanjaa21@users.noreply.github.com>
2025-09-29 19:05:37 +0200

    Update OcrFileIngestModule.py

--- OcrFileIngestModule.py
+++ OcrFileIngestModule.py
@@ -11,11 +11,8 @@ from javax.swing import JCheckBox
 from javax.swing import BoxLayout
 from javax.swing import JPanel
 from javax.swing import JLabel
-from javax.swing import JTextField
-from javax.swing import JRadioButton
-from javax.swing import ButtonGroup
-from java.awt import GridBagLayout
-from java.awt import GridBagConstraints
+from javax.swing import JComboBox
+from java.awt import Component
 from org.sleuthkit.autopsy.casemodule import Case
 from org.sleuthkit.autopsy.casemodule.services import Services
 from org.sleuthkit.autopsy.ingest import DataSourceIngestModule
@@ -84,7 +81,7 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
         self.ingestServices = IngestServices.getInstance()
         self.filesFound = 0
         self.context = None
-
+    
     def startUp(self, context):
         self.context = context
         if self.local_settings.getSetting("jpg_flag") == "true":
@@ -115,7 +112,6 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
             temp_file_path = None
             processed_file_path = None
             try:
-                #  Save the original image to a temporary file
                 temp_fd, temp_file_path = tempfile.mkstemp(suffix=os.path.splitext(file.getName())[1])
                 os.close(temp_fd)
                 
@@ -130,21 +126,17 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
                 
                 self.log(Level.INFO, "Temporary original image file saved to: " + temp_file_path)
 
-                #  Apply image preprocessing with ImageMagick
                 processed_fd, processed_file_path = tempfile.mkstemp(suffix=".png")
                 os.close(processed_fd)
 
-                # Dynamically build the command based on user options
                 magick_args = []
                 magick_args.append('magick')
                 magick_args.append(temp_file_path)
                 
-                # Grayscale
                 if self.local_settings.getSetting("grayscale_flag") == "true":
                     magick_args.append('-grayscale')
                     magick_args.append('Rec709Luminance')
 
-                # Resize
                 if self.local_settings.getSetting("skip_resize_flag") == "false":
                     resize_value = self.local_settings.getSetting("resize_value")
                     if resize_value:
@@ -163,7 +155,6 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
                 
                 self.log(Level.INFO, "Image preprocessed with ImageMagick and saved to: " + processed_file_path)
 
-                #  Run Tesseract on the processed image
                 tesseract_cmd = ['tesseract', processed_file_path, 'stdout']
                 
                 language_code = self.local_settings.getSetting("language_code")
@@ -179,11 +170,8 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
                     self.log(Level.WARNING, "Tesseract failed to process file " + file.getName() + ". Error: " + tesseract_stderr)
                     return IngestModule.ProcessResult.OK
 
-                #  Decode the stdout byte stream to a UTF-8 string
                 ocr_text = tesseract_stdout.decode('utf-8', 'ignore').strip()
                 self.log(Level.INFO, "OCR text extracted from " + file.getName())
-                self.log(Level.INFO, "Text which was extracted ====== " + ocr_text )
-                self.log(Level.INFO, "==================================")
 
                 if ocr_text:
                     self.filesFound += 1
@@ -223,7 +211,6 @@ class OcrFileIngestModuleWithUI(FileIngestModule):
             except Exception as e:
                 self.log(Level.SEVERE, "An unexpected error occurred during OCR: " + str(e))
             finally:
-                #  Clean up both temporary files
                 if temp_file_path and os.path.exists(temp_file_path):
                     os.remove(temp_file_path)
                 if processed_file_path and os.path.exists(processed_file_path):
@@ -266,88 +253,52 @@ class OcrFileIngestModuleWithUISettingsPanel(IngestModuleIngestJobSettingsPanel)
     def checkboxSkipResizeEvent(self, event):
         skip_resize_flag = self.checkboxSkipResize.isSelected()
         self.local_settings.setSetting("skip_resize_flag", "true" if skip_resize_flag else "false")
-        
-        # Enable/disable radio buttons based on the checkbox state
-        self.resize25.setEnabled(not skip_resize_flag)
-        self.resize50.setEnabled(not skip_resize_flag)
-        self.resize75.setEnabled(not skip_resize_flag)
-        self.resize100.setEnabled(not skip_resize_flag)
-        
-        # Set a default value if the checkbox is unchecked
+        self.resizeCombo.setEnabled(not skip_resize_flag)
         if not skip_resize_flag:
             current_resize_value = self.local_settings.getSetting("resize_value")
             if not current_resize_value:
-                self.resize100.setSelected(True)
+                self.resizeCombo.setSelectedItem("100%")
                 self.local_settings.setSetting("resize_value", "100")
 
-    def resizeEvent(self, event):
-        self.local_settings.setSetting("resize_value", event.getSource().getText().strip('%'))
-    
-    #  Event handler for language radio buttons
-    def languageEvent(self, event):
-        if self.language_eng.isSelected():
+    def resizeComboEvent(self, event):
+        value = self.resizeCombo.getSelectedItem().replace('%', '')
+        self.local_settings.setSetting("resize_value", value)
+
+    def languageComboEvent(self, event):
+        selected = self.languageCombo.getSelectedItem()
+        if "eng" in selected:
             self.local_settings.setSetting("language_code", "eng")
-        elif self.language_srp.isSelected():
+        elif "srp" in selected:
             self.local_settings.setSetting("language_code", "srp")
-        elif self.language_deu.isSelected():
+        elif "deu" in selected:
             self.local_settings.setSetting("language_code", "deu")
+        elif "fra" in selected:
+            self.local_settings.setSetting("language_code", "fra")
+        elif "spa" in selected:
+            self.local_settings.setSetting("language_code", "spa")
 
     def initComponents(self):
         self.setLayout(BoxLayout(self, BoxLayout.Y_AXIS))
-        
-        self.image_types_label = JLabel("Image file formats:")
-        self.checkboxJpg = JCheckBox("Process JPG/JPEG images", actionPerformed=self.checkboxJpgEvent)
-        self.checkboxPng = JCheckBox("Process PNG images", actionPerformed=self.checkboxPngEvent)
-        self.checkboxTif = JCheckBox("Process TIFF images", actionPerformed=self.checkboxTifEvent)
-        self.checkboxBmp = JCheckBox("Process BMP images", actionPerformed=self.checkboxBmpEvent)
-        self.checkboxGif = JCheckBox("Process GIF images", actionPerformed=self.checkboxGifEvent)
-        
-        self.preprocessing_label = JLabel("Image Preprocessing Options:")
-        
-        # Grayscale
-        self.checkboxGrayscale = JCheckBox("Grayscale", actionPerformed=self.checkboxGrayscaleEvent)
 
-        # Skip Resize Checkbox
-        self.checkboxSkipResize = JCheckBox("Skip Resize", actionPerformed=self.checkboxSkipResizeEvent)
-        
-        # Resize radio buttons
-        self.resizeLabel = JLabel("Resize:")
-        self.resizeGroup = ButtonGroup()
-        self.resize25 = JRadioButton("25%", actionPerformed=self.resizeEvent)
-        self.resize50 = JRadioButton("50%", actionPerformed=self.resizeEvent)
-        self.resize75 = JRadioButton("75%", actionPerformed=self.resizeEvent)
-        self.resize100 = JRadioButton("100%", actionPerformed=self.resizeEvent)
+        self.image_types_label = JLabel("Image file formats:"); self.image_types_label.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxJpg = JCheckBox("Process JPG/JPEG images", actionPerformed=self.checkboxJpgEvent); self.checkboxJpg.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxPng = JCheckBox("Process PNG images", actionPerformed=self.checkboxPngEvent); self.checkboxPng.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxTif = JCheckBox("Process TIFF images", actionPerformed=self.checkboxTifEvent); self.checkboxTif.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxBmp = JCheckBox("Process BMP images", actionPerformed=self.checkboxBmpEvent); self.checkboxBmp.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxGif = JCheckBox("Process GIF images", actionPerformed=self.checkboxGifEvent); self.checkboxGif.setAlignmentX(Component.LEFT_ALIGNMENT)
 
-        self.resizeGroup.add(self.resize25)
-        self.resizeGroup.add(self.resize50)
-        self.resizeGroup.add(self.resize75)
-        self.resizeGroup.add(self.resize100)
+        self.preprocessing_label = JLabel("Image Preprocessing Options:"); self.preprocessing_label.setAlignmentX(Component.LEFT_ALIGNMENT)
 
-        self.resizePanel = JPanel()
-        self.resizePanel.setLayout(BoxLayout(self.resizePanel, BoxLayout.X_AXIS))
-        self.resizePanel.add(self.resizeLabel)
-        self.resizePanel.add(self.resize25)
-        self.resizePanel.add(self.resize50)
-        self.resizePanel.add(self.resize75)
-        self.resizePanel.add(self.resize100)
+        self.checkboxGrayscale = JCheckBox("Grayscale", actionPerformed=self.checkboxGrayscaleEvent); self.checkboxGrayscale.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.checkboxSkipResize = JCheckBox("Skip Resize", actionPerformed=self.checkboxSkipResizeEvent); self.checkboxSkipResize.setAlignmentX(Component.LEFT_ALIGNMENT)
 
-        # Language options
-        self.language_label = JLabel("Language for Tesseract:")
-        self.languageGroup = ButtonGroup()
-        self.language_eng = JRadioButton("English (eng)", actionPerformed=self.languageEvent)
-        self.language_srp = JRadioButton("Serbian (srp)", actionPerformed=self.languageEvent)
-        self.language_deu = JRadioButton("German (deu)", actionPerformed=self.languageEvent)
+        self.resizeLabel = JLabel("Resize:"); self.resizeLabel.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.resizeCombo = JComboBox(["25%", "50%", "75%", "100%"]); self.resizeCombo.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.resizeCombo.addActionListener(self.resizeComboEvent)
 
-        self.languageGroup.add(self.language_eng)
-        self.languageGroup.add(self.language_srp)
-        self.languageGroup.add(self.language_deu)
-        
-        self.languagePanel = JPanel()
-        self.languagePanel.setLayout(BoxLayout(self.languagePanel, BoxLayout.X_AXIS))
-        self.languagePanel.add(self.language_label)
-        self.languagePanel.add(self.language_eng)
-        self.languagePanel.add(self.language_srp)
-        self.languagePanel.add(self.language_deu)
+        self.language_label = JLabel("Language for Tesseract:"); self.language_label.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.languageCombo = JComboBox(["English (eng)", "Serbian (srp)", "German (deu)", "French (fra)", "Spanish (spa)"]); self.languageCombo.setAlignmentX(Component.LEFT_ALIGNMENT)
+        self.languageCombo.addActionListener(self.languageComboEvent)
 
         self.add(self.image_types_label)
         self.add(self.checkboxJpg)
@@ -355,18 +306,19 @@ class OcrFileIngestModuleWithUISettingsPanel(IngestModuleIngestJobSettingsPanel)
         self.add(self.checkboxTif)
         self.add(self.checkboxBmp)
         self.add(self.checkboxGif)
-        
+
         self.add(JPanel())
-        
+
         self.add(self.preprocessing_label)
         self.add(self.checkboxGrayscale)
         self.add(self.checkboxSkipResize)
-        self.add(self.resizePanel)
-        
+        self.add(self.resizeLabel)
+        self.add(self.resizeCombo)
+
         self.add(JPanel())
-        
-        # Add language panel to the UI
-        self.add(self.languagePanel)
+
+        self.add(self.language_label)
+        self.add(self.languageCombo)
 
     def customizeComponents(self):
         self.checkboxJpg.setSelected(self.local_settings.getSetting("jpg_flag") == "true")
@@ -374,45 +326,31 @@ class OcrFileIngestModuleWithUISettingsPanel(IngestModuleIngestJobSettingsPanel)
         self.checkboxTif.setSelected(self.local_settings.getSetting("tif_flag") == "true")
         self.checkboxBmp.setSelected(self.local_settings.getSetting("bmp_flag") == "true")
         self.checkboxGif.setSelected(self.local_settings.getSetting("gif_flag") == "true")
-        
+
         self.checkboxGrayscale.setSelected(self.local_settings.getSetting("grayscale_flag") == "true")
 
         skip_resize_flag = self.local_settings.getSetting("skip_resize_flag") == "true"
         self.checkboxSkipResize.setSelected(skip_resize_flag)
-        
-        # Set the enabled state of resize radio buttons
-        self.resize25.setEnabled(not skip_resize_flag)
-        self.resize50.setEnabled(not skip_resize_flag)
-        self.resize75.setEnabled(not skip_resize_flag)
-        self.resize100.setEnabled(not skip_resize_flag)
+        self.resizeCombo.setEnabled(not skip_resize_flag)
 
-        # Set the selected resize radio button based on saved settings
         resize_value = self.local_settings.getSetting("resize_value")
-        if not resize_value or skip_resize_flag:
-            # Default to 100% if no value is saved or if skipping resize
-            self.resize100.setSelected(True)
-        elif resize_value == "25":
-            self.resize25.setSelected(True)
-        elif resize_value == "50":
-            self.resize50.setSelected(True)
-        elif resize_value == "75":
-            self.resize75.setSelected(True)
+        if resize_value in ["25", "50", "75", "100"]:
+            self.resizeCombo.setSelectedItem(resize_value + "%")
         else:
-            self.resize100.setSelected(True)
-        
-        #  Set the selected language radio button
+            self.resizeCombo.setSelectedItem("100%")
+
         language_code = self.local_settings.getSetting("language_code")
-        if not language_code:
-            # Default to English
-            self.language_eng.setSelected(True)
-            self.local_settings.setSetting("language_code", "eng")
-        elif language_code == "eng":
-            self.language_eng.setSelected(True)
-        elif language_code == "srp":
-            self.language_srp.setSelected(True)
+        if language_code == "srp":
+            self.languageCombo.setSelectedItem("Serbian (srp)")
         elif language_code == "deu":
-            self.language_deu.setSelected(True)
+            self.languageCombo.setSelectedItem("German (deu)")
+        elif language_code == "fra":
+            self.languageCombo.setSelectedItem("French (fra)")
+        elif language_code == "spa":
+            self.languageCombo.setSelectedItem("Spanish (spa)")
+        else:
+            self.languageCombo.setSelectedItem("English (eng)")
+            self.local_settings.setSetting("language_code", "eng")
 
     def getSettings(self):
         return self.local_settings
-
